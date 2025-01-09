6591dd38e5a9d2e7f51b3e8145a789d73597cde7
therjyer <sekimperium@gmail.com>
2025-01-09 10:11:42 -0300

    name module

--- name.py
+++ name.py
@@ -1,14 +1,21 @@
 import re
 
-pattern = r"^[A-Z][a-z]+(?: [A-Z][a-z]+)+$"
-
-while True:
-    nome = input("Digite o nome: ")
-
-    if nome.lower() == 'exit':
-        break
-
+def verificar_nome(nome):
+    pattern = r"^[A-Z][a-z]+(?: [A-Z][a-z]+)+$"
+    
     if re.match(pattern, nome):
         print("Nome válido")
     else:
         print("Nome inválido")
+
+def main():
+    while True:
+        nome = input("Digite o nome (ou 'exit' para sair): ")
+
+        if nome.lower() == 'exit':
+            break
+        
+        verificar_nome(nome)
+
+if __name__ == "__main__":
+    main()
