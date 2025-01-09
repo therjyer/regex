cb8a9e8e7193bcc3e500f1b1f2fa56198388098b
therjyer <sekimperium@gmail.com>
2025-01-09 10:10:14 -0300

    cpf

---
+++ cpf.py
@@ -0,0 +1,16 @@
+import re
+
+def verificar_cpf(cpf):
+    pattern = r"^\d{3}\.\d{3}\.\d{3}-\d{2}$"
+    
+    if re.match(pattern, cpf):
+        print("CPF válido")
+    else:
+        print("CPF inválido")
+
+def main():
+    cpf = input("Digite o CPF (formato xxx.xxx.xxx-xx): ")
+    verificar_cpf(cpf)
+
+if __name__ == "__main__":
+    main()
